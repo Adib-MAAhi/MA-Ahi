87c6001eee42236d023338289c62d74e6d35f334
Adib-MAAhi <adibmaahi83@gamil.com>
2024-08-20 10:38:19 +0600

    Add new button and input element

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # MA-Ahi
+
 For 1st time create repos
 <br/>
 Author : MA Ahi
+Published: 8/20/2024
---
+++ React.jsx
@@ -0,0 +1,10 @@
+function React() {
+  return (
+    <div>
+      <input type="text" placeholder="..search" />
+      <button>Add</button>
+    </div>
+  );
+}
+
+export default React;
