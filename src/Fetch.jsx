036ca4f61e95d594302515953db6743d41f308c2
Adib-MAAhi <adibmaahi83@gamil.com>
2024-08-23 15:03:10 +0600

    changes

---
+++ src/Fetch.jsx
@@ -0,0 +1,33 @@
+import styles from "./Fetch.module.css";
+import { Suspense, use } from "react";
+import Loading from "./Loading";
+
+const fetchJokes = async () => {
+  const res = await fetch("https://api.chucknorris.io/jokes/random");
+  return res.json();
+};
+
+const JokeItem = () => {
+  const joke = use(fetchJokes());
+
+  return (
+    <div className={styles.joke}>
+      <h2>{joke.value}</h2>
+    </div>
+  );
+};
+export default function Joke() {
+  return (
+    <div>
+      <Suspense
+        fallback={
+          <div className={styles.loder}>
+            <Loading />
+          </div>
+        }
+      >
+        <JokeItem />
+      </Suspense>
+    </div>
+  );
+}
